e232407a62b458d1f8e6dad4169f0e620373aaad
Timothy Meade <zt.tmzt@gmail.com>
2010-10-28 14:08:45 -0400

    cleanup androix.c

--- hw/android/jni/androix.c
+++ hw/android/jni/androix.c
@@ -9,13 +9,9 @@ static pthread_mutex_t miEventQueueMutex = PTHREAD_MUTEX_INITIALIZER;
 
 jint
 JNI_OnLoad(JavaVM *jvm, void *reserved) {
-//    xandroid_jvm = jvm; // pass to kdandroid driver
-//    LOG("xandroid_jvm: %.8x", xandroid_jvm);
-
     Android = (AndroidVars *)calloc(sizeof(AndroidVars), 1);
     Android->miEventQueueMutex = &miEventQueueMutex;
     androidInitNative(jvm);
-
     return JNI_VERSION_1_4;
 }
 
@@ -29,19 +25,13 @@ Java_net_homeip_ofn_androix_AndroiXLib_init( JNIEnv* env, jobject thiz )
     char envp[] = {};
 
     LOG("fixing up /data/data/net.homeip.ofn.androix/usr/bin/xkbcomp");
-
     chmod("/data/data/net.homeip.ofn.androix/usr/bin/xkbcomp", 0775);
-
     LOG("done.");
 
-    stat("/data/data/net.homeip.ofn.androix/usr/bin/xkbcomp", &stats);
-
-    LOG("/data/data/net.homeip.ofn.androix/usr/bin/xkbcomp mode: %o" + stats.st_mode);
-
     LOG("creating event wakeup socket pair");
-    socketpair(AF_UNIX, SOCK_STREAM, 0, &(Android->wakeupFD));
-    LOG("remote (Xorg) socket: %d", Android->wakeupFD[0]);
-    LOG("local (android) socket: %d", Android->wakeupFD[1]);
+    socketpair(AF_UNIX, SOCK_STREAM, 0, Android->wakeupFD);
+    LOG("remote (Xorg) fd: %d", Android->wakeupFD[0]);
+    LOG("local (android) fd: %d", Android->wakeupFD[1]);
     
     LOG("starting DIX");
 	dix_main(1, &argv, &envp);
@@ -54,7 +44,7 @@ void wakeupFD() {
 	//  <daniels> oh, i ... er ... christ.
     LOG("writing to wakeupFD");
 	res = write(Android->wakeupFD[1], &nullbyte, sizeof(nullbyte));
-//  res = write(Android->wakeupFD[1], "X", 1);
+    //res = write(Android->wakeupFD[1], "X", 1);
     LOG("wrote %d bytes", res);
 }
 
@@ -62,57 +52,31 @@ void
 Java_net_homeip_ofn_androix_AndroiXLib_keyDown( JNIEnv* env, jobject thiz, jint kbd, jint keyCode )
 {
     LOG("keyDown: kbd: %p keyCode: %d", kbd, keyCode);
-    wakeupFD();
-
-    //androidRequestInputLock();
-//    LOG("keyDown: taking miEventQueueMutex");
-//    pthread_mutex_lock(Android->miEventQueueMutex);
     androidCallbackKeyDown((void *)kbd, keyCode);
     wakeupFD();
-    //androidReleaseInputLock();
-//    LOG("keyDown: releasing miEventQueueMutex");
-//    pthread_mutex_unlock(Android->miEventQueueMutex);
 }
 
 void
 Java_net_homeip_ofn_androix_AndroiXLib_keyUp( JNIEnv* env, jobject thiz, jint kbd, jint keyCode )
 {
     LOG("keyUp: kbd: %p keyCode: %d", (unsigned int)kbd, keyCode);
-    //androidRequestInputLock();
-//    LOG("keyUp: taking miEventQueueMutex");
-//    pthread_mutex_lock(Android->miEventQueueMutex);
     androidCallbackKeyUp((void *)kbd, keyCode);
     wakeupFD();
-    //androidReleaseInputLock();
-//    LOG("keyUp: releasing miEventQueueMutex");
-//    pthread_mutex_unlock(Android->miEventQueueMutex);
 }
 
 void
 Java_net_homeip_ofn_androix_AndroiXLib_touchDown( JNIEnv* env, jobject thiz, jint mouse, jint x, jint y )
 {
     LOG("touchDown: mouse: %p x: %d y: %d", mouse, x, y);
-    //androidRequestInputLock();
-//    LOG("touchDown: taking miEventQueueMutex");
-//    pthread_mutex_lock(Android->miEventQueueMutex);
     androidCallbackTouchDown((void *)mouse, x, y);
     wakeupFD();
-    //androidReleaseInputLock();
-//    LOG("touchDown: releasing miEventQueueMutex");
-//    pthread_mutex_unlock(Android->miEventQueueMutex);
 }
 
 void
 Java_net_homeip_ofn_androix_AndroiXLib_touchUp( JNIEnv* env, jobject thiz, jint mouse, jint x, jint y )
 {
     LOG("touchUp: mouse: %p x: %d y: %d", mouse, x, y);
-    //androidRequestInputLock();
-//    LOG("touchUp: taking miEventQueueMutex");
-//    pthread_mutex_lock(Android->miEventQueueMutex);
     androidCallbackTouchUp((void *)mouse, x, y);
     wakeupFD();
-    //androidReleaseInputLock();
-//    LOG("touchUp: releasing miEventQueueMutex");
-//    pthread_mutex_unlock(Android->miEventQueueMutex);
 }
 
--- hw/android/old-package.sh
+++
@@ -1,5 +0,0 @@
-cp /opt/androix/usr/lib/libxandroid.a obj/local/armeabi/
-/home/tmzt/src/android-ndk-4/android-ndk-r4b/ndk-build
-cp usrdata.zip libs/armeabi/libusrdata.so
-ant compile
-ant debug
